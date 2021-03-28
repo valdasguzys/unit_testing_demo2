69ffa45e99a7836972880e86f12cf20be26a56b7
Valdas Gužys <valdas.guzys@gmail.com>
2021-03-28 21:00:13 +0300

    added mock test

--- tests/unit/EmployeeControllerTest.php
+++ tests/unit/EmployeeControllerTest.php
@@ -10,19 +10,37 @@ use function PHPUnit\Framework\assertEquals;
 
 class EmployeeContollerTest extends TestCase {
 
+    // test with database
     public function test1() {
         //given
-        //$employeeRepository = new EmployeeRepository();
-        $stub = $this->createStub(EmployeeRepository::class);
-        $stub->method('getAll')->willReturn(array(new Employee(1, "Petras"), new Employee(2, "Jonas")));
-
-        $employeeController = new EmployeeController($stub);
-
+        //$employeeRepository takes data from database
+        $employeeRepository = new EmployeeRepository();
+        $employeeController = new EmployeeController($employeeRepository);
         //when
         $res = $employeeController->getAllJson(); 
-
         //then
+        assertEquals('[{"id":"1","name":"Petras"},{"id":"2","name":"Jonas"}]', $res);
+    }
+
+    //stub test
+    public function test2() {
+        $stub = $this->createStub(EmployeeRepository::class);
+        $stub->method('getAll')->willReturn(array(new Employee(1, "Petras"), new Employee(2, "Jonas")));
+        $employeeController = new EmployeeController($stub);
+
+        $res = $employeeController->getAllJson(); 
 
         assertEquals('[{"id":1,"name":"Petras"},{"id":2,"name":"Jonas"}]', $res);
+    }
+
+    //mock test
+    public function testGetAllJsonReturnsJson() {
+        $mock = $this->getMockBuilder(EmployeeRepository::class)->getMock();
+        $employeeController = new EmployeeController($mock);
+        $mock->expects($this->exactly(1))->method('getAll')->willReturn(array(new Employee(1, "Petras"), new Employee(2, "Jonas")));
+
+        $res = $employeeController->getAllJson(); 
+
+        assertEquals('[{"id":1,"name":"Petras"},{"id":2,"name":"Jonas"}]', $res); 
     }
 }
