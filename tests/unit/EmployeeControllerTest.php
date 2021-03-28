ca4f0447b474d1f7897609d69725ea887cdf2773
Valdas Gužys <valdas.guzys@gmail.com>
2021-03-28 20:34:05 +0300

    added stub for testing

--- tests/unit/EmployeeControllerTest.php
+++ tests/unit/EmployeeControllerTest.php
@@ -1,6 +1,9 @@
 <?php declare(strict_types=1);
 
+
+
 use Controller\EmployeeController;
+use Model\Employee;
 use PHPUnit\Framework\TestCase;
 use Repository\EmployeeRepository;
 use function PHPUnit\Framework\assertEquals;
@@ -9,14 +12,17 @@ class EmployeeContollerTest extends TestCase {
 
     public function test1() {
         //given
-        $employeeRepository = new EmployeeRepository();
-        $employeeController = new EmployeeController($employeeRepository);
+        //$employeeRepository = new EmployeeRepository();
+        $stub = $this->createStub(EmployeeRepository::class);
+        $stub->method('getAll')->willReturn(array(new Employee(1, "Petras"), new Employee(2, "Jonas")));
+
+        $employeeController = new EmployeeController($stub);
 
         //when
         $res = $employeeController->getAllJson(); 
 
         //then
 
-        assertEquals('[{"id":"1","name":"Petras"},{"id":"2","name":"Jonas"}]', $res);
+        assertEquals('[{"id":1,"name":"Petras"},{"id":2,"name":"Jonas"}]', $res);
     }
 }
