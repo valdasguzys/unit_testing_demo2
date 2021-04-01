9e1240f221bb845e3fde5bfb8ba9b38f399a269c
Valdas Gužys <valdas.guzys@gmail.com>
2021-04-01 20:20:05 +0300

    tests with faker generated data

--- tests/unit/EmployeeControllerTest.php
+++ tests/unit/EmployeeControllerTest.php
@@ -3,6 +3,7 @@
 
 
 use Controller\EmployeeController;
+use Faker\Factory;
 use Model\Employee;
 use PHPUnit\Framework\TestCase;
 use Repository\EmployeeRepository;
@@ -35,13 +36,45 @@ class EmployeeContollerTest extends TestCase {
 
     //mock test
     public function testGetAllJsonReturnsJson() {
+        // $mock = $this->getMockBuilder(EmployeeRepository::class)->getMock();
+        // $employeeController = new EmployeeController($mock);
+        // $mock->expects($this->exactly(1))->method('getAll')->willReturn(array(new Employee(1, "Petras"), new Employee(2, "Jonas")));
+
+        // $res = $employeeController->getAllJson(); 
+
+        // assertEquals('[{"id":1,"name":"Petras"},{"id":2,"name":"Jonas"}]', $res); 
+        //---------------------------------
+
+        // data created with faker library
+        // $faker = Factory::create();
+        // $name1 = $faker->name();
+        // $name2 = $faker->name();
+
+        // $mock = $this->getMockBuilder(EmployeeRepository::class)->getMock();
+        // $employeeController = new EmployeeController($mock);
+        // $mock->expects($this->exactly(1))
+        //     ->method('getAll')
+        //     ->willReturn(array(
+        //         new Employee(1, $name1),
+        //         new Employee(1, $name2)
+        //     ));
+        // $res = $employeeController->getAllJson();
+        // assertEquals('[{"id":1,"name":"' . $name1 . '"},{"id":1,"name":"' . $name2 . '"}]', $res);
+        
+        //a bit more complicated faker example
+        $faker = Factory::create();
+        $employees = array();
+        for ($i=0; $i < 10; $i++ )
+            array_push($employees, new Employee($i, $faker->name()));
         $mock = $this->getMockBuilder(EmployeeRepository::class)->getMock();
         $employeeController = new EmployeeController($mock);
-        $mock->expects($this->exactly(1))->method('getAll')->willReturn(array(new Employee(1, "Petras"), new Employee(2, "Jonas")));
+            $mock->expects($this->exactly(1))
+                ->method('getAll')
+                ->willREturn($employees);
+        
+        $res = $employeeController->getAllJson();
+        assertEquals(json_encode($employees), $res);
 
-        $res = $employeeController->getAllJson(); 
-
-        assertEquals('[{"id":1,"name":"Petras"},{"id":2,"name":"Jonas"}]', $res); 
     }
     
 
